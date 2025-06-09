238275d121e925bbd1c942fa2c936daa2b74a75a
Anushka <as0846403@gmail.com>
2025-06-09 19:15:26 +0530

    fixing cors

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use(express.json());
 
 app.use("/api/form", formRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors(
+//   {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }
+));
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: "Something went wrong!" });
