67d56759414deb01622e789583efac6fb05c9db0
Anushka <as0846403@gmail.com>
2025-06-09 18:35:03 +0530

    adding env and cors

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 
 app.use("/api/form", formRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: "Something went wrong!" });
