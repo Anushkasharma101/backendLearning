cac529f7583d3a37ac315f084df169a8ea5ce9d2
Anushka <as0846403@gmail.com>
2025-06-09 19:23:02 +0530

    fixed location of cors

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-
-const formRoutes = require("./routes/formRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-
-app.use(express.json());
-
-app.use("/api/form", formRoutes);
-app.use("/api/tasks", taskRoutes);
 app.use(cors(
 //   {
 //   origin: '*',
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // }
 ));
+app.use(express.json());
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: "Something went wrong!" });
-});
+const formRoutes = require("./routes/formRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+
+app.use("/api/form", formRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ error: "Something went wrong!" });
+// });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
