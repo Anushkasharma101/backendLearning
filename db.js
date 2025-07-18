25b7af6d69402aeb8fa07013354ddb82a3c3b5c0
Anushka <as0846403@gmail.com>
2025-07-19 02:51:45 +0530

    fixing deployment on render

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
