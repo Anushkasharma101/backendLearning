fa96c2a77d02deab3d5dfc8ec5f6d78797f8d293
Anushka <as0846403@gmail.com>
2025-07-19 05:37:45 +0530

    neon service added

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NEON_URL,
   ssl: {
     require: true,             // ✅ Force SSL
     rejectUnauthorized: false  // ✅ Ignore self-signed cert issue
